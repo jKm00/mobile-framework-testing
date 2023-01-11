bbbb2f9f346433af887e66563a407fd36dfec47f
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2023-01-11 23:33:33 +0100

    Started on todo app

--- MyReacApp/App.tsx
+++ MyReacApp/App.tsx
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+interface Todo {
+  id: string
+  title: string
+  completed: boolean
+}
 
 export default function App() {
+
+  const [input, onChangeText] = useState("");
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handlePress = () => console.log('handle press');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputSection}>
+        <TextInput style={styles.input} onChangeText={onChangeText} value={input} placeholder={'Enter todo title...'} />
+        <Button title='Add todo' onPress={handlePress} />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +31,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 30,
   },
+  inputSection: {
+    flexDirection: 'row',
+    gap: 1,
+  },
+  input: {
+    flex: 1,
+    borderColor: '#000',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    padding: 5,
+  }
 });
